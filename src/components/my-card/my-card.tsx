1b67137144bff58dad7805a283c165d8fce5fd72
Github Projects <barik011@gmail.com>
2025-05-19 11:13:37 +0530

    tab content changes

--- src/components/my-card/my-card.tsx
+++ src/components/my-card/my-card.tsx
@@ -6,17 +6,24 @@ import { Component, h, State } from "@stencil/core";
     shadow: true,
 })
 export class MyCard{
-    @State() showStencilContent: boolean = true;
-
-    tabContent(){
-        this.showStencilContent = false;
+    @State() showStencilContent: boolean = false;
+    @State() showReactContent: boolean = true;
+    tabContent(content:string){
+        if(content === 'stencil'){
+            this.showStencilContent = true;
+            this.showReactContent = false;
+        }
+        else if(content === 'react'){
+            this.showReactContent = true;
+            this.showStencilContent = false;
+        }
+        else{
+            this.showReactContent = false;
+            this.showStencilContent = false;
+        }
     }
 
-    displayTabContent:string='';
-
-    if(this.showStencilContent){
-        this.displayTabContent = stencil;
-    }
+    
 
 
     render(){
@@ -30,15 +37,23 @@ export class MyCard{
                         <button class="stencil-btn">Stencil Button</button>
                       </div>);
 
+        let displayTabContent='';
+        if(this.showStencilContent){
+            displayTabContent = stencil;
+        }
+        else if(this.showReactContent){
+            displayTabContent = react;
+        }
+
         let myContainer = (           
                 <div class="card">
                     <div class="card-header">
                         <h5>My Main Card</h5>
                     </div>
                     <div class="card-content">
-                        <button class="react-btn" onClick={this.tabContent.bind(this)}>React</button>
-                        <button class="stencil-btn" onClick={this.tabContent.bind(this)}>Stencil</button>
-                        {this.showStencilContent?react:stencil}
+                        <button class="react-btn" onClick={this.tabContent.bind(this,'react')}>React</button>
+                        <button class="stencil-btn" onClick={this.tabContent.bind(this,'stencil')}>Stencil</button>
+                        {displayTabContent}
                     </div>
                 </div>           
         );
